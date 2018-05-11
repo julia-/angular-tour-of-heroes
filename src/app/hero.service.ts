0adec6ae62ec37ffb8c4969f0a5c9c8de8104664
julia- <julia.ryanx@gmail.com>
2018-05-11 11:51:31 +1000

    create MessageComponent and update HeroService to inject into HeroComponent with MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core'
+
 import { Observable, of } from 'rxjs'
+
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
+import { MessageService } from './message.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  // constructor receives a param that declares a private messageService property
+  // singleton MessageService is injected into that property when the HeroService is created
+  // typical "service-in-service" scenario - inject the MessageService into the HeroService which is injected into the HeroesComponent.
+  constructor(private messageService: MessageService) {}
+
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES)
   }
-  constructor() {}
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service'
 
@@ -8,13 +9,9 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[]
-
   selectedHero: Hero
 
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes))
-  }
+  heroes: Hero[]
 
   constructor(private heroService: HeroService) {}
 
@@ -24,5 +21,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => (this.heroes = heroes))
   }
 }
