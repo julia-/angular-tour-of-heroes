225b176c786c367e914ce2cbc6975834b0e54be6
julia- <julia.ryanx@gmail.com>
2018-05-11 14:55:30 +1000

    tidy structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms' // <-- NgModel lives here
 
 import { AppComponent } from './app.component'
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module'
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { HeroesComponent } from './heroes/heroes.component'
+import { MessagesComponent } from './messages/messages.component'
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
