1549cfcfff140e20c808dda28dfce893453445d4
julia- <julia.ryanx@gmail.com>
2018-05-11 11:49:48 +1000

    create MessageService to add and clear messages

---
+++ src/app/message.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  messages: string[] = []
+
+  // add message to the cache
+  add(message: string) {
+    this.messages.push(message)
+  }
+
+  // clears the cache
+  clear() {
+    this.messages = []
+  }
+
+  constructor() { }
+}
